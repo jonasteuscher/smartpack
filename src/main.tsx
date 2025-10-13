9c2711ebf746f67d3527435fcd5febd6fa208f43
jonasteuscher <jonas.teuscher@gmail.com>
2025-10-13 13:04:08 +0200

    🐛 update import statement on main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import './i18n.ts';
+import './i18n';
 import './index.css';
 import { ThemeProvider } from './context/ThemeContext';
 import { AuthProvider } from './context/AuthContext';
